2edc35e34b77d4728c5427773594011e5d04abd1
kimjhj11 <kimjhj11@naver.com>
2021-10-03 22:51:03 +0900

    fix: 코드 수정
    코드 인코딩을 utf-8로 변경

--- Project/MayBreaker_Project/Assets/Scripts/MainCamera.cs
+++ Project/MayBreaker_Project/Assets/Scripts/MainCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCamera_Action : MonoBehaviour
+public class MainCamera : MonoBehaviour
 {
     public GameObject player;
 
@@ -18,13 +18,13 @@ public class MainCamera_Action : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void LateUpdate()
