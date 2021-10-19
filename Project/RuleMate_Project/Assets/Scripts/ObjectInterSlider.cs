7b9724a61b69339c2b11e4d95dc38e60d15cf02c
kimjhj11 <kimjhj11@naver.com>
2021-10-20 04:10:05 +0900

    test : z 누르면 상호작용 진행바 표시 테스트

---
+++ Project/RuleMate_Project/Assets/Scripts/ObjectInterSlider.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInterSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    float progress = 0;
+    float progressMIN = 0;
+    float progressMAX = 100;
+
+    //public GameObject player;
+
+    //미션중이면~ 나중에 추가
+    private bool isMissioned = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.minValue = progressMIN;
+        slider.maxValue = progressMAX;
+        slider.value = progress;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            progress += 0.25f;
+        }
+        else
+        {
+            if(progress >= 0)
+                progress -= 0.25f;
+        }
+
+        if (progress > 0 && isMissioned == true)
+            slider.gameObject.SetActive(true);
+        else
+            slider.gameObject.SetActive(false);
+
+        //slider.transform.position = Camera.main.WorldToScreenPoint(this.transform.position + new Vector3(0, 0.8f, 0));
+    }
+}
