214594563beab7c3ebf4b5cd79ad38c2a409f956
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-12-05 03:11:17 +0900

    feat : SoundManager.cs - 사운드 매니저 로직 추가, 배경음악 관리

---
+++ Project/RuleMate_Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    Dictionary<string, AudioSource> Songs;
+    public string TitleSong;
+    public string LobbySong;
+    public string InGameSong;
+    public string EventSong;
+    public string ResultSong;
+
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            return;
+        DontDestroyOnLoad(gameObject);
+
+        Songs = new Dictionary<string, AudioSource>();
+
+        AudioSource[] audioSources = GetComponentsInChildren<AudioSource>();
+        foreach (var song in audioSources)
+            Songs.Add(song.name, song);
+
+        Songs[TitleSong].Play();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        foreach (var song in Songs)
+            song.Value.Stop();
+
+        if (scene.name == "Local_InGame")
+            Songs[InGameSong].Play();
+        else if(scene.name == "Main_Lobby_Room")
+            Songs[TitleSong].Play();
+        else if (scene.name == "EventScene")
+            Songs[EventSong].Play();
+    }
+
+    public void PlayResultSong()
+    {
+        foreach (var song in Songs)
+            song.Value.Stop();
+
+        Songs[ResultSong].Play();
+    }
+}
